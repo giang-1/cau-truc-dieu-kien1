b2721eb8549cedb3fe6b0ea8224145cde021ed64
giang-1 <giang06121102@gmail.com>
2023-11-17 19:41:45 +0700

    cap nhat 1

--- bai4/bai4.js
+++ bai4/bai4.js
@@ -1,14 +1,30 @@
-let a = prompt('nhập a')
-let b = prompt('nhập b')
-let c = prompt('nhập c')
-if (a > b) {
-    if (a >c){
-        alert('a là số lớn nhất')
-    } else {
-        alert('c là số lớn nhất')
+let a = +prompt('nhập a')
+let b = +prompt('nhập b')
+let c = +prompt('nhập c')
+// if (a > b) {
+//     if (a > c){
+//         alert('a là số lớn nhất')
+//     } else {
+//         alert('c là số lớn nhất')
+//     }
+// }else if (b > c) {
+//     alert('b là số lớn nhất')
+// }else{
+//     alert('c là số lớn nhất')
+// }
+// if ( a > b && a > c ) {
+//     alert('a là số lớn nhất')
+// }else if (b > a && b > c) {
+//     alert('b là số lớn nhất')
+// }else {
+//     alert('c là số lớn nhất')
+// }
+let max_num;
+if ( a > b && a > c ) {
+    max_num = a
+    }else if (b > a && b > c) {
+        max_num = b
+    }else {
+        max_num = c
     }
-}else if (b > c) {
-    alert('b là số lớn nhất')
-}else{
-    alert('c là số lớn nhất')
-}
+    alert(`${max_num} là số lớn nhất`)
--- bai5/bai5.js
+++ bai5/bai5.js
@@ -1,5 +1,13 @@
-let a = prompt('nhập điểm kiểm tra:')
-let b = prompt('nhập điểm thi giữa kỳ :')
-let c = prompt('nhập điểm thi cuối kỳ :')
+let a = +prompt('nhập điểm kiểm tra:')
+let b = +prompt('nhập điểm thi giữa kỳ :')
+let c = +prompt('nhập điểm thi cuối kỳ :')
 let d = ((a * 2) + (b * 3) + (c * 6)) / 11
-alert(d)
+if (d >= 8 && d <= 10) {
+    alert('giỏi')
+}else if (d < 8 && d >= 6.5) {
+    alert('khá')
+}else if ( d < 6.5 && d >= 5){
+    alert('trung bình')
+}else{
+    alert('yếu')
+}
